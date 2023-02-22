00777a55800acd96a0786fa6781ec146d4f31b1c
MayurgitHub14 <mayurmhadght14@gmail.com>
2023-02-22 16:02:49 +0530

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,29 @@
+import MailList from "../../components/mailList/MailList";
+import Featured from "../../components/featured/Featured";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import PropertyList from "../../components/propertyList/PropertyList";
+import "./home.css";
+import Footer from "../../components/footer/Footer";
+
+
+function Home() {
+  return (
+    <div>
+        <Navbar />
+        <Header />
+        <div className="homeContainer">
+          <Featured/>
+          <h1 className="homeTitle">Browse the property type</h1>
+          <PropertyList/>
+          <h1 className="homeTitle">Homes guests love</h1>
+          <FeaturedProperties />
+          <MailList/>
+          <Footer/>
+        </div>
+    </div>
+  );
+};
+
+export default Home
